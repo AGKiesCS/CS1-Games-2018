73600892e1b5985cf6884befde769ce12b7085c9
RobloxKid <38662783+RobloxKid@users.noreply.github.com>
2018-04-24 09:44:38 -0500

    Adding Earth Protectors

---
+++ Earth Protectors/Spaceship.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Spaceship here.
+ * 
+ * @author (IsmaelSalinas) 
+ * @version (a version number or a date)
+ */
+public class Spaceship extends Actor
+{
+    /**
+     * Act - do whatever the Spaceship wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+       // When pressing up thrust forward
+       // When pressing down thrust backward
+       // When pressing left go left
+       // When pressing right go right
+       
+       // When pressing space Shoot laser
+       
+       
+       
+       
+       
+        
+        
+        
+        
+        
+        
+    }    
+}
