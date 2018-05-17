6beae16854dd9f93f9a625a393d874d35eb645c5
JamBaJB <38663242+JamBaJB@users.noreply.github.com>
2018-05-17 10:06:57 -0500

    Add files via upload

---
+++ Corsa 3/Corsa 3/LapCounter.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LapCounter here.
+ * 
+ * @author (James Barham and Connor Hayes) 
+ * @version (a version number or a date)
+ */
+public class LapCounter extends Actor
+{
+    /**
+     * Act - do whatever the LapCounter wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        //Show how many laps have been completed out of 50
+    }    
+}
