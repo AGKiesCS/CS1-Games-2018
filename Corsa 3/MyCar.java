473a2ca0f3a7fbe213d0290f35081a730ee8208b
JamBaJB <38663242+JamBaJB@users.noreply.github.com>
2018-05-29 10:56:35 -0500

    Add files via upload
    Final Update

--- Corsa 3/MyCar.java
+++ Corsa 3/MyCar.java
@@ -8,53 +8,45 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyCar extends Cars
 {
-   GreenfootSound EngineStart = new GreenfootSound("CarEngineStart.mp3");
+    GreenfootSound EngineStart = new GreenfootSound("CarEngineStart.mp3");
     GreenfootSound EngineRunning = new GreenfootSound("CarEngineRunning.mp3");
- 
-    
-    
+
     /**
      * Act - do whatever the MyCar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-   public MyCar()
-   {  
-       super();
-       getImage(). rotate(90);
+    public MyCar()
+    {  
+        super();
+        getImage(). rotate(90);
     }
-    
-    
-    
+
     public void act() 
     {
-        
+
         if(Greenfoot.isKeyDown("up"))
         {
-           move(2);
-           EngineStart.play();
-           EngineRunning.playLoop();
+            move(3);
+            EngineStart.play();
+            EngineRunning.playLoop();
         } 
-    else{
-        EngineRunning.pause();
-    }
-         if(Greenfoot.isKeyDown("down"))
+        else{
+            EngineRunning.pause();
+        }
+        if(Greenfoot.isKeyDown("down"))
         {
-           move (-2);
-    }    
-         if(Greenfoot.isKeyDown("right"))
+            move (-3);
+        }    
+        if(Greenfoot.isKeyDown("right"))
         {
-           turn(3);
-    }    
-         if(Greenfoot.isKeyDown("left"))
+            turn(3);
+        }    
+        if(Greenfoot.isKeyDown("left"))
         {
-           turn(-3);
-    }    
-    if (isTouching(DEleteMw.class))
-    {
-            removeTouching(DEleteMw.class);
+            turn(-3);
+        }    
     }
 
-
+    
 }
 
-}
